767c4f41f8c6f643a4b7a79e8a2fc7174a9abd94
isolanilla <i.solanilla3@gmail.com>
2016-04-12 13:20:52 +0200

    primera version

---
+++ flick.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
+
+  $.getJSON(flickerAPI, {
+		tags: "fuenlabrada",
+		tagmode: "any",
+		format: "json"
+    })
+
+  .done(function(data) {
+			$.each(data.items, function(i, item) {
+				$("<img>").attr("src", item.media.m).appendTo("#images");
+
+			});
+	});
+});
